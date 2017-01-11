53254aab315354e5615bf245089e80bbd686235c
Markvanlierop <mark-vl@live.nl>
2017-01-11 22:28:53 +0100

    asd

---
+++ EyeCT4EventsMVC/EyeCT4EventsMVC/Controllers/ReserveringController.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="ReserveringController.cs" company="Unitech">
+//     Company copyright tag.
+// </copyright>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EyeCT4EventsMVC.Models.Persistencies;
+using EyeCT4EventsMVC.Models.Repositories;
+
+namespace EyeCT4EventsMVC.Controllers
+{
+    public class ReserveringController : Controller
+    {
+        public RepositoryKampeerPlaatsen Rkp = new RepositoryKampeerPlaatsen(new MSSQLReserveren());
+
+        // GET: Reservering
+        public ActionResult Reservering()
+        {
+            ViewBag.AlleKampeerplaatsen = Rkp.AlleKampeerplaatsenOpvragen();           
+            return View();
+        }
+    }
+}
